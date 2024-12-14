d8bf4a3976fc08f0d29f75261b359181d2f2126f
Aran96-V <59768554+Aran96-V@users.noreply.github.com>
2024-12-14 14:54:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.use((req, res, next) => {
     res.status(404).send('404: Page Not Found');
 });
 
-// Start the server
+// Dynamically set the port for Render or fallback to 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
